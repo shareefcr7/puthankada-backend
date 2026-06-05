439d43366c8c7df8182d0c5b51e9166636de2ae3
Muhammed Shareef <shareefnncr@gmail.com>
2026-06-05 14:55:33 +0530

    chore: add root GET / handler and seed:admin script

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+// Root route to show API is running (prevents "Cannot GET /" in browser)
+router.get('/', (req, res) => res.send('Puthan-Kada backend is running. Visit /api for endpoints'));
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
